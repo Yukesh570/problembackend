5bb09c5644766a24d974b65218b3815026f78207
yukesh570 <yukesh.maha10@gmail.com>
2024-08-07 10:31:02 +0545

    checkcamera

--- MyApi/check_cameras.py
+++ MyApi/check_cameras.py
@@ -1,19 +1,33 @@
+# import cv2
+
+# def get_available_cameras(max_cameras=10):
+#     available_cameras = []
+#     for i in range(max_cameras):
+#         cap = cv2.VideoCapture(i)
+#         if cap.isOpened():
+#             available_cameras.append(i)
+#             cap.release()
+#     return available_cameras
+
+# if __name__ == "__main__":
+#     cameras = get_available_cameras()
+#     print(f"Available cameras: {len(cameras)}")
+#     print("Camera indexes:", cameras)
+
+
+
+#     #just for testing
+
+
 import cv2
+import numpy
 
-def get_available_cameras(max_cameras=10):
-    available_cameras = []
-    for i in range(max_cameras):
-        cap = cv2.VideoCapture(i)
-        if cap.isOpened():
-            available_cameras.append(i)
-            cap.release()
-    return available_cameras
-
-if __name__ == "__main__":
-    cameras = get_available_cameras()
-    print(f"Available cameras: {len(cameras)}")
-    print("Camera indexes:", cameras)
-
-
-
-    #just for testing
+capture= cv2.VideoCapture(0)
+while True:
+    ret,frame =capture.read()
+    cv2.imshow('output',frame)
+    k=cv2.waitKey(100) 
+    if k==ord('q'):
+        break
+capture.release()
+cv2.destroyAllWindows()
