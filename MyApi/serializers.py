3df44e49a8dcdecaa799b6ee5e252fb68c188309
yukesh570 <yukesh.maha10@gmail.com>
2024-08-20 11:17:36 +0545

    space manage

--- MyApi/serializers.py
+++ MyApi/serializers.py
@@ -1,14 +1,9 @@
 from rest_framework import serializers
 from .models import Person ,Table
-
-
 class PersonaldataSerializer(serializers.ModelSerializer):
     class Meta:
         model=Person
         fields='__all__'
-
-
-
 
 class TableSerializer(serializers.ModelSerializer):
     person = PersonaldataSerializer()
